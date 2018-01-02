60b0e2b46a1b7f81bf17eb85198e9d5153e654a0
Sahil Kharya <kharyasahil@gmail.com>
2018-01-02 19:05:01 +0530

    new

--- main.js
+++ main.js
@@ -17,3 +17,32 @@ $(function() {
         event.preventDefault();
     });
 });
+
+
+var DocApp = angular.module('DocApp',[]);
+
+foodieApp.controller('mainController',function($scope) {
+    //$scope is an object to which we can add a list of properties and functions
+    // the prop. or function which are present in html file can be accessed though '$scope.property' and $scope can be used inside the controller in which it is defined.
+    $scope.doctors = [{
+        name: 'Dr Shivam Jar',
+        degree: 'BVM',
+        experience: '4 Years',
+        Specialty: 'Veterinarians',
+        location: 'Sitapura',
+        cost: '200',
+        hours: '10 AM to 8 PM (Mon-Sat)',
+        image: 'https://b.zmtcdn.com/data/pictures/3/1401633/f076bdb7e3fda3a87ba182207862632c.jpg',
+        id: '1'
+    },
+
+    {   name: 'Dr Shivam Jar',
+        degree: 'BVM',
+        experience: '4 Years',
+        Specialty: 'Veterinarians',
+        location: 'Sitapura',
+        cost: '200',
+        hours: '10 AM to 8 PM (Mon-Sat)',
+        id: '2'
+    }]
+})
